efa1d3f3bd775c3338a67f0d304e97062c55f116
Henrique Eidt <henriqueidt@gmail.com>
2026-03-24 10:36:29 -0300

    fix lists styles

--- poc-performance-profiling/app/components/SlowList.tsx
+++ poc-performance-profiling/app/components/SlowList.tsx
@@ -1,6 +1,6 @@
 const SlowList = ({ items }: { items: string[] }) => {
   return (
-    <ul className="flex ">
+    <ul className="flex flex-col h-40 overflow-y-auto border border-gray-200 dark:border-gray-700 rounded p-2 w-full">
       {items.map((item) => (
         <li key={item}>{item}</li>
       ))}
--- poc-performance-profiling/app/welcome/welcome.tsx
+++ poc-performance-profiling/app/welcome/welcome.tsx
@@ -9,7 +9,7 @@ export function Welcome() {
 
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+      <div className="flex-1 flex items-center gap-16 min-h-0">
         <ProfilerPanel id="Slowlist">
           <SlowList items={items} />
         </ProfilerPanel>
